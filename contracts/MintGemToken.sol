0793f3f0a6c57b9e1e5a2da68eda937e12758b30
wisysta <choi199042@gmail.com>
2022-06-12 18:20:04 +0900

    3rd

--- contracts/MintGemToken.sol
+++ contracts/MintGemToken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
+// token
 contract MintGemToken is ERC721Enumerable {
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
