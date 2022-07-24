ea379ad56179934581c1f486b97c75ee3ddc648e
wisysta <choi199042@gmail.com>
2022-07-24 14:48:00 +0900

    메타데이터 추가

--- contracts/MintGemToken.sol
+++ contracts/MintGemToken.sol
@@ -5,12 +5,37 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract MintGemToken is ERC721Enumerable {
-    constructor(string memory _name, string memory _symbol)
-        ERC721(_name, _symbol)
-    {}
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _metadataURI
+    ) ERC721(_name, _symbol) {
+        metadataURI = _metadataURI;
+    }
+
+    struct GemTokenData {
+        uint256 gemTokenRank;
+        uint256 gemTokenType;
+    }
+
+    mapping(uint256 => GemTokenData) public gemTokenData;
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        string memory gemTokenRank = String.toString(
+            gemTokenData[_tokenId].gemTokenRank
+        );
+        string memory gemTokenType = String.toString(
+            gemTokenData[_tokenId].gemTokenType
+        );
+    }
 
     function mintGemToken() public {
-        uint tokenId = totalSupply() + 1;
+        uint256 tokenId = totalSupply() + 1;
 
         _mint(msg.sender, tokenId);
     }
